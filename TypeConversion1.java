85ad73dd2560f3bd246fa1978b2a251e6d18702a
Deepak Yadav <65231118+Deepak2k20@users.noreply.github.com>
2022-12-17 14:41:27 +0530

    Add files via upload

---
+++ TypeConversion1.java
@@ -0,0 +1,14 @@
+
+public class TypeConversion1 {
+
+	public static void main(String[] args) {
+		
+		int num1=100;
+		float f= num1; // Widening
+		
+		System.out.println(num1);
+		System.out.print(f);
+
+	}
+
+}
