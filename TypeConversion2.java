d05664eb9ea57e56ab306ea27f65ce7df1d540cc
Deepak Yadav <65231118+Deepak2k20@users.noreply.github.com>
2022-12-17 14:43:55 +0530

    Add files via upload

---
+++ TypeConversion2.java
@@ -0,0 +1,12 @@
+
+public class TypeConversion2 {
+
+	public static void main(String args[])
+	{
+		float f= 10.5f;
+		int num= (int) f; // Narrowing(Typecasting)
+		
+		System.out.println(f);
+		System.out.print(num);
+	}
+}
