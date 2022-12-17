4776f6c35dfb291680a69d5c9a79a6a0714a9b20
Deepak Yadav <65231118+Deepak2k20@users.noreply.github.com>
2022-12-17 14:33:17 +0530

    Add files via upload

---
+++ VariableDemo.java
@@ -0,0 +1,17 @@
+
+public class VariableDemo {
+		
+		static int num1=100; // static variable
+		
+		void method()
+		{
+			int num2=200; // local variable
+		}
+		
+		public static void main(String args[])
+		{
+			int num3=300; // instance variable
+		}
+
+}
+
