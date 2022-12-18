d4c4a38672d9dfbb5c6dfc1fd665f78e322f21d0
Deepak Yadav <65231118+Deepak2k20@users.noreply.github.com>
2022-12-18 09:01:41 +0000

    commit

---
+++ UnaryOperator.java
@@ -0,0 +1,10 @@
+class UnaryOperator{
+    public static void main(String args[])
+    {
+        int num = 10;
+        System.out.println(num++); // 10
+        System.out.println(++num); // 12
+        System.out.println(num--); // 12
+        System.out.println(--num); // 10
+    }
+}
