99db2baa42fcb0147d2d12bbeebf044ea4cf3fef
Deepak Yadav <65231118+Deepak2k20@users.noreply.github.com>
2022-12-18 15:49:28 +0530

    Add files via upload

---
+++ IfElseDemo.java
@@ -0,0 +1,19 @@
+
+public class IfElseDemo {
+
+	public static void main(String[] args) {
+		
+		int age = 21;
+		
+		if(age >= 18)
+		{
+			System.out.print("You're eligible to vote!");
+		}
+		else
+		{
+			System.out.print("You're not eligible to vote!");
+		}
+
+	}
+
+}
