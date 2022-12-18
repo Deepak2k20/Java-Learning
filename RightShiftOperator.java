4ed475faefb40e49d93b7801f0cad0f170007b97
Deepak Yadav <65231118+Deepak2k20@users.noreply.github.com>
2022-12-18 14:53:53 +0530

    Add files via upload

---
+++ RightShiftOperator.java
@@ -0,0 +1,11 @@
+
+public class RightShiftOperator {
+
+	public static void main(String args[])
+	{
+		int num1 = 60;
+		
+		System.out.println(num1>>2); // num1/2^2
+		System.out.println(num1>>3);
+	}
+}
