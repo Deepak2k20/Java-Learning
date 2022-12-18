ede1ff05d19a8a84a56aa242b6d8b4d6665f9331
Deepak Yadav <65231118+Deepak2k20@users.noreply.github.com>
2022-12-18 14:47:23 +0530

    Add files via upload

---
+++ UnaryOperatorDemo3.java
@@ -0,0 +1,18 @@
+
+public class UnaryOperatorDemo3 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		int num2 = -10;
+		boolean a = true;
+		boolean b = false;
+		
+		System.out.println(~num1);
+		System.out.println(~num2);
+		System.out.println(!a);
+		System.out.println(!b);
+
+	}
+
+}
