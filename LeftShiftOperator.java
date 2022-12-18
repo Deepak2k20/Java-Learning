80ec96105a93e202c74aad13c0a2a6e751087e00
Deepak Yadav <65231118+Deepak2k20@users.noreply.github.com>
2022-12-18 14:52:20 +0530

    Add files via upload

---
+++ LeftShiftOperator.java
@@ -0,0 +1,13 @@
+
+public class LeftShiftOperator {
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		
+		System.out.println(num1<<2); // num1 * 2^2
+		System.out.println(num1<<3);
+
+	}
+
+}
