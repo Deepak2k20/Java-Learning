ee0fd021d4331c3996fab3ee0626207809ede039
Deepak Yadav <65231118+Deepak2k20@users.noreply.github.com>
2022-12-18 15:28:29 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,14 @@
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		
+		int num1 = 10;
+		int num2 = 20;
+		
+		int min = num1 < num2 ? num1 : num2;
+		System.out.print(min);
+
+	}
+
+}
