918c5dc4a436f3d54cb2a1ee803dac7e0ecaf9a1
Deepak Yadav <65231118+Deepak2k20@users.noreply.github.com>
2022-12-30 16:26:29 +0530

    Add files via upload

---
+++ SwitchDemo.java
@@ -0,0 +1,23 @@
+
+public class SwitchDemo {
+
+	public static void main(String[] args) {
+		
+		int num=40;
+		
+		switch(num)
+		{
+		case 10: System.out.println("10");
+		break;
+		case 20: System.out.println("20");
+		break;
+		case 30: System.out.println("30");
+		break;
+		case 40: System.out.println("40");
+		break;
+		default:System.out.println("Not in 10, 20 , 30 or 40");
+		}
+
+	}
+
+}
