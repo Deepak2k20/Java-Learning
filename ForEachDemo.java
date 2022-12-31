e2c1fcf354f9c760c7a645fa110bb64915fe1d2e
Deepak Yadav <65231118+Deepak2k20@users.noreply.github.com>
2022-12-31 15:51:45 +0530

    Add files via upload

---
+++ ForEachDemo.java
@@ -0,0 +1,15 @@
+
+public class ForEachDemo {
+
+	public static void main(String[] args) {
+		
+		int arr[] = {1,2,3,4,5};
+		
+		for(int num : arr)
+		{
+			System.out.println(num);
+		}
+
+	}
+
+}
